96520df4c715708d01e41295f8edd62668c032af
Mateuzs <mateusz.zembol@gmail.com>
2019-03-02 19:44:07 +0100

    changing elixir version

--- infinibird_engine/mix.exs
+++ infinibird_engine/mix.exs
@@ -5,7 +5,7 @@ defmodule InfinibirdEngine.MixProject do
     [
       app: :infinibird_engine,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
