f854083e51382f68213482f2202b132b0552c8c0
Mateuzs <mateusz.zembol@gmail.com>
2019-03-04 08:11:29 +0100

    config

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
+
+import Chartkick from "chartkick";
+// do JS stuff
+window.Chartkick = Chartkick;
+
 import css from "../css/app.css";
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -8,9 +13,3 @@ import css from "../css/app.css";
 //
 // Import dependencies
 import "phoenix_html";
-import Chartkick from "chartkick";
-
-// Import local files
-
-// do JS stuff
-window.Chartkick = Chartkick;
