ca5f2e94f75b99ada7d47068a5dc4135acfafe77
Mateuzs <mateusz.zembol@gmail.com>
2019-03-28 16:34:48 +0100

    https gigalixir config

--- config/prod.exs
+++ config/prod.exs
@@ -59,7 +59,7 @@ config :logger, level: :info
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-config :infinibird, InfinibirdWeb.Endpoint, force_ssl: [hsts: true]
+config :infinibird, InfinibirdWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
