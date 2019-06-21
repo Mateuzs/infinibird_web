eaa55d723e86361aea07fda0335f88d01f80cca5
Mateuzs <mateusz.zembol@gmail.com>
2019-06-21 17:37:53 +0200

    modification of static assets path

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -36,6 +36,6 @@ module.exports = (env, options) => ({
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: "../css/app.css" }),
-    new CopyWebpackPlugin([{ from: "static/", to: "../static" }])
+    new CopyWebpackPlugin([{ from: "static/", to: "../" }])
   ]
 });
