def0c997e8f9200b5e172e18e67715b6a5dc81c2
Mateuzs <mateusz.zembol@gmail.com>
2019-10-10 19:34:24 +0200

    extended basic charts config

--- lib/infinibird/charts_config/charts_config.ex
+++ lib/infinibird/charts_config/charts_config.ex
@@ -1,4 +1,10 @@
 defmodule Infinibird.ChartsConfig do
+  @spec getChartsConfig :: %{
+          area_charts: [map, ...],
+          column_charts: [map, ...],
+          line_charts: [map, ...],
+          pie_charts: [map, ...]
+        }
   def getChartsConfig() do
     %{
       column_charts: [
@@ -12,7 +18,34 @@ defmodule Infinibird.ChartsConfig do
           data_extractor: fn -> nil end
         }
       ],
-      line_charts: [],
+      line_charts: [
+        %{
+          description: %{
+            title: "Przejechane kilometry w ostatnich 7 dniach",
+            vAxis: %{title: "Liczba kilometrów"},
+            hAxis: %{title: "Dzień"},
+            colors: ["purple"]
+          },
+          data_extractor: fn -> nil end
+        },
+        %{
+          description: %{
+            title: "Liczba przejazdów w ostatnich 7 dniach",
+            vAxis: %{title: "Liczba przejazdów"},
+            hAxis: %{title: "Dzień"},
+            colors: ["red"]
+          },
+          data_extractor: fn -> nil end
+        },
+        %{
+          description: %{
+            title: "Przejechane kilometry na przestrzeni 3 miesięcy",
+            vAxis: %{title: "Liczba kilometrów"},
+            hAxis: %{title: "Dzień"}
+          },
+          data_extractor: fn -> nil end
+        }
+      ],
       area_charts: [
         %{
           description: %{
