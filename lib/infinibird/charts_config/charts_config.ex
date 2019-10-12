613f7c10de20935e234ff7e4eb8d2ab5e6e3ede1
Mateuzs <mateusz.zembol@gmail.com>
2019-10-12 21:39:19 +0200

    charts dynamic config implementation

--- lib/infinibird/charts_config/charts_config.ex
+++ lib/infinibird/charts_config/charts_config.ex
@@ -1,11 +1,11 @@
 defmodule Infinibird.ChartsConfig do
-  @spec getChartsConfig :: %{
+  @spec get_charts_config :: %{
           area_charts: [map, ...],
           column_charts: [map, ...],
           line_charts: [map, ...],
           pie_charts: [map, ...]
         }
-  def getChartsConfig() do
+  def get_charts_config() do
     %{
       column_charts: [
         %{
@@ -15,7 +15,83 @@ defmodule Infinibird.ChartsConfig do
             hAxis: %{title: "Prędkość w km/h"},
             colors: ["blue"]
           },
-          data_extractor: fn -> nil end
+          data_extractor: fn data ->
+            distance = Enum.reduce(data, 0, fn e, acc -> acc + e["distance_m"] end)
+
+            [
+              %{
+                name: "Procent czasu w przedziale prędkości",
+                data: [
+                  [
+                    "1-25km/h",
+                    (Enum.reduce(
+                       data,
+                       0,
+                       fn e, acc ->
+                         acc + e["distance_m_speed_below_25_kmh"]
+                       end
+                     ) / (distance / 100))
+                    |> Float.round(2)
+                  ],
+                  [
+                    "26-50km/h",
+                    (Enum.reduce(
+                       data,
+                       0,
+                       fn e, acc ->
+                         acc + e["distance_m_speed_25_50_kmh"]
+                       end
+                     ) / (distance / 100))
+                    |> Float.round(2)
+                  ],
+                  [
+                    "51-75km/h",
+                    (Enum.reduce(
+                       data,
+                       0,
+                       fn e, acc ->
+                         acc + e["distance_m_speed_50_75_kmh"]
+                       end
+                     ) / (distance / 100))
+                    |> Float.round(2)
+                  ],
+                  [
+                    "76-100km/h",
+                    (Enum.reduce(
+                       data,
+                       0,
+                       fn e, acc ->
+                         acc + e["distance_m_speed_75_100_kmh"]
+                       end
+                     ) / (distance / 100))
+                    |> Float.round(2)
+                  ],
+                  [
+                    "101-125km/h",
+                    (Enum.reduce(
+                       data,
+                       0,
+                       fn e, acc ->
+                         acc + e["distance_m_speed_100_125_kmh"]
+                       end
+                     ) / (distance / 100))
+                    |> Float.round(2)
+                  ],
+                  [
+                    ">125km/h",
+                    (Enum.reduce(
+                       data,
+                       0,
+                       fn e, acc ->
+                         acc + e["distance_m_speed_over_125_kmh"]
+                       end
+                     ) / (distance / 100))
+                    |> Float.round(2)
+                  ]
+                ]
+              }
+            ]
+          end
         }
       ],
       line_charts: [
@@ -26,7 +102,54 @@ defmodule Infinibird.ChartsConfig do
             hAxis: %{title: "Dzień"},
             colors: ["purple"]
           },
-          data_extractor: fn -> nil end
+          data_extractor: fn data ->
+            week_data =
+              Enum.sort_by(data, fn ride ->
+                d = Date.from_iso8601!(ride["date"])
+                {d.year, d.month, d.day}
+              end)
+              |> Enum.filter(fn ride ->
+                Date.compare(Date.from_iso8601!(ride["date"]), Date.add(Date.utc_today(), -6)) !==
+                  :lt
+              end)
+              |> Enum.group_by(fn ride -> ride["date"] end)
+
+            [
+              %{
+                name: "kilometry",
+                data: %{
+                  "#{Date.add(Date.utc_today(), -6)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -6)}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["distance_m"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).(),
+                  "#{Date.add(Date.utc_today(), -5)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -5)}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["distance_m"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).(),
+                  "#{Date.add(Date.utc_today(), -4)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -4)}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["distance_m"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).(),
+                  "#{Date.add(Date.utc_today(), -3)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -3)}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["distance_m"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).(),
+                  "#{Date.add(Date.utc_today(), -2)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -2)}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["distance_m"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).(),
+                  "#{Date.add(Date.utc_today(), -1)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -1)}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["distance_m"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).(),
+                  "#{Date.utc_today()}":
+                    Map.get(week_data, "#{Date.utc_today()}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["distance_m"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).()
+                }
+              }
+            ]
+          end
         },
         %{
           description: %{
@@ -35,7 +158,47 @@ defmodule Infinibird.ChartsConfig do
             hAxis: %{title: "Dzień"},
             colors: ["red"]
           },
-          data_extractor: fn -> nil end
+          data_extractor: fn data ->
+            week_data =
+              Enum.sort_by(data, fn ride ->
+                d = Date.from_iso8601!(ride["date"])
+                {d.year, d.month, d.day}
+              end)
+              |> Enum.filter(fn ride ->
+                Date.compare(Date.from_iso8601!(ride["date"]), Date.add(Date.utc_today(), -6)) !==
+                  :lt
+              end)
+              |> Enum.group_by(fn ride -> ride["date"] end)
+
+            [
+              %{
+                name: "przejazdy",
+                data: %{
+                  "#{Date.add(Date.utc_today(), -6)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -6)}", [])
+                    |> length(),
+                  "#{Date.add(Date.utc_today(), -5)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -5)}", [])
+                    |> length(),
+                  "#{Date.add(Date.utc_today(), -4)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -4)}", [])
+                    |> length(),
+                  "#{Date.add(Date.utc_today(), -3)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -3)}", [])
+                    |> length(),
+                  "#{Date.add(Date.utc_today(), -2)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -2)}", [])
+                    |> length(),
+                  "#{Date.add(Date.utc_today(), -1)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -1)}", [])
+                    |> length(),
+                  "#{Date.utc_today()}":
+                    Map.get(week_data, "#{Date.utc_today()}", [])
+                    |> length()
+                }
+              }
+            ]
+          end
         },
         %{
           description: %{
@@ -43,7 +206,7 @@ defmodule Infinibird.ChartsConfig do
             vAxis: %{title: "Liczba kilometrów"},
             hAxis: %{title: "Dzień"}
           },
-          data_extractor: fn -> nil end
+          data_extractor: fn data -> [] end
         }
       ],
       area_charts: [
@@ -55,7 +218,54 @@ defmodule Infinibird.ChartsConfig do
             colors: ["green"],
             pointSize: 5
           },
-          data_extractor: fn -> nil end
+          data_extractor: fn data ->
+            week_data =
+              Enum.sort_by(data, fn ride ->
+                d = Date.from_iso8601!(ride["date"])
+                {d.year, d.month, d.day}
+              end)
+              |> Enum.filter(fn ride ->
+                Date.compare(Date.from_iso8601!(ride["date"]), Date.add(Date.utc_today(), -6)) !==
+                  :lt
+              end)
+              |> Enum.group_by(fn ride -> ride["date"] end)
+
+            [
+              %{
+                name: "minuty",
+                data: %{
+                  "#{Date.add(Date.utc_today(), -6)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -6)}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["tavel_time_minutes"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).(),
+                  "#{Date.add(Date.utc_today(), -5)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -5)}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["tavel_time_minutes"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).(),
+                  "#{Date.add(Date.utc_today(), -4)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -4)}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["tavel_time_minutes"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).(),
+                  "#{Date.add(Date.utc_today(), -3)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -3)}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["tavel_time_minutes"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).(),
+                  "#{Date.add(Date.utc_today(), -2)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -2)}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["tavel_time_minutes"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).(),
+                  "#{Date.add(Date.utc_today(), -1)}":
+                    Map.get(week_data, "#{Date.add(Date.utc_today(), -1)}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["tavel_time_minutes"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).(),
+                  "#{Date.utc_today()}":
+                    Map.get(week_data, "#{Date.utc_today()}", [])
+                    |> Enum.reduce(0, fn e, acc -> acc + e["tavel_time_minutes"] end)
+                    |> (fn e -> Kernel.trunc(e / 1000) end).()
+                }
+              }
+            ]
+          end
         }
       ],
       pie_charts: [
@@ -65,7 +275,18 @@ defmodule Infinibird.ChartsConfig do
             colors: ["green", "orange"]
           },
           donut: true,
-          data_extractor: fn -> nil end
+          data_extractor: fn data ->
+            [
+              [
+                "Liczba przyspieszeń",
+                Enum.reduce(data, 0, fn e, acc -> acc + e["accelerations"] end)
+              ],
+              [
+                "Liczba zahmowań",
+                Enum.reduce(data, 0, fn e, acc -> acc + e["decelerations"] end)
+              ]
+            ]
+          end
         },
         %{
           description: %{
@@ -73,7 +294,18 @@ defmodule Infinibird.ChartsConfig do
             colors: ["purple", "cyan"]
           },
           donut: true,
-          data_extractor: fn -> nil end
+          data_extractor: fn data ->
+            [
+              [
+                "Liczba skrętów w lewo",
+                Enum.reduce(data, 0, fn e, acc -> acc + e["left_turns"] end)
+              ],
+              [
+                "Liczba skrętów w prawo",
+                Enum.reduce(data, 0, fn e, acc -> acc + e["right_turns"] end)
+              ]
+            ]
+          end
         }
       ]
     }
