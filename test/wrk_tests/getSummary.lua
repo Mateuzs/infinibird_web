7f9180530d18cb821ed7b6ec2715131f6c353505
Mateuzs <mateusz.zembol@gmail.com>
2019-11-11 11:03:16 +0100

    added more perf tests

---
+++ test/wrk_tests/getSummary.lua
@@ -0,0 +1,3 @@
+wrk.method = "GET"
+wrk.headers["Authorization"] = "Basic YzNWd1pYSmZjMlZqY21WMFgzVnpaWEk9OmNHRnpjM2R2Y21SZmMzVndaWEpmYzJWamNtVjA="
+wrk.headers["Content-Type"] = "application/json"
--- test/wrk_tests/new_trip.lua
+++
@@ -1,4 +0,0 @@
-wrk.method = "POST"
-wrk.body = "{\"deviceId\": '9bac2143-3f85-44f6-ad56-b575549af9e4', \"rideId\": '20190725T190508_20190725T193610.json'}"
-wrk.headers["Content-Type"] = "application/json"
-wrk.headers["Authorization"] = " Basic YzNWd1pYSmZjMlZqY21WMFgzVnpaWEk9OmNHRnpjM2R2Y21SZmMzVndaWEpmYzJWamNtVjA="
