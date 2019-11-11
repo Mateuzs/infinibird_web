abd4f08858e4e0a24ae31db2fd2c70d246462b6a
Mateuzs <mateusz.zembol@gmail.com>
2019-11-11 12:34:43 +0100

    fixing

--- lib/infinibird/data_provider/data_provider.ex
+++ lib/infinibird/data_provider/data_provider.ex
@@ -43,7 +43,7 @@ defmodule Infinibird.DataProvider do
 
     HTTPoison.get!(
       "#{Application.get_env(:infinibird, :infinibird_service_url)}/infinibird/trips/#{device_id}",
-      [{"content-type", "application/bson"}, {"Authorization", "Basic #{credentials}"}],
+      [{"content-type", "text/bson"}, {"Authorization", "Basic #{credentials}"}],
       stream_to: self(),
       async: :once,
       timeout: 50_000,
